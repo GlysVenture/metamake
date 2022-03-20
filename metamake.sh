aa083dea7f3ecedde5233e38d7a59eb1de8bbd73
Tadeusz Kondracki <tkondrac@student.42lausanne.ch>
2022-03-20 18:59:13 +0100

    git stuff

--- metamake.sh
+++ metamake.sh
@@ -8,9 +8,9 @@ fi
 if [ $1 = --update ] ; then
 	git -C $HOME/.metamake restore $HOME/.metamake > /dev/null
 	if git -C $HOME/.metamake pull > /dev/null ; then
-		echo "error: failed to update metamake"
-	else
 		echo "metamake successfully updated"
+	else
+		echo "error: failed to update metamake"
 	fi
 	exit
 fi
