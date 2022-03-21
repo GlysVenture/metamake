4aae211fed2738aa80550d5c06906a2590493f11
Tadeusz Kondracki <tadeusz.kondracki@gmail.com>
2022-03-21 10:46:40 +0100

    error management

--- metamake.sh
+++ metamake.sh
@@ -8,9 +8,9 @@ fi
 if [ $1 = --update ] ; then
 	git -C $HOME/.metamake restore $HOME/.metamake > /dev/null
 	if git -C $HOME/.metamake pull > /dev/null ; then
-		echo "[`tput setaf 6 && tput bold`metamake`tput sgr0`] already up to date"
+		echo "[`tput setaf 6 && tput bold`metamake`tput sgr0`] up to date!"
 	else
-		echo "[`tput setaf 6 && tput bold`metamake`tput sgr0`] update successfull"
+		echo "`tput setaf 1 && tput bold`error`tput sgr0`: git pull error"
 	fi
 	exit
 fi
